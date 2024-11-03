e309a11c36ba2496c38f06b2ddf1a67772539069
Alina Lozovskaya <alinalozov445@gmail.com>
2024-11-04 02:47:03 +0400

    fix cors

--- core/src/src/cruds/crud_credentials.py
+++ core/src/src/cruds/crud_credentials.py
@@ -1,45 +1,28 @@
 from pydantic import ValidationError
 from sqlalchemy.orm import Session
 
-from models.schemas import CredentialsModel
-from models.models import Credentials
-from cruds import crud_users
+from src.models.schemas import CredentialsModel
+from src.models.models import Credentials
 
 def get_credentials_by_credentials_id(db: Session, credentials_id: int) -> Credentials:
-    return db.query(Credentials).filter(Credentials.credentials_id == credentials_id).first()
+    return None
 
 
 def get_credentials_by_user_id(db: Session, user_id: int) -> Credentials:
-    return db.query(Credentials).filter(Credentials.user_id == user_id).first()
+    return None
 
 
 def get_credentials_id_by_login(db: Session, login : str) -> Credentials:
-    return db.query(Credentials).filter(Credentials.login == login).first()
+    return None
 
 
 def get_user_id_by_login(db: Session, login : str) -> int:
-    credentials = db.query(Credentials).filter(Credentials.login == login).first()
-    if credentials is not None:
-        return credentials.user_id
     return None
 
 
 def create_user(db: Session, credentials: CredentialsModel) -> int:
-    user_id = crud_users.create_user(db)
-    try:
-        db_cred = Credentials(user_id=user_id, login=credentials.login, 
-                                password_hash=credentials.password_hash)
-    except ValidationError:
-        return None
-    db.add(db_cred)
-    db.commit()
-    db.refresh(db_cred)
-    return db_cred.user_id
+    return 0
 
 
 def delete_credentials(db: Session, credentials_id: int) -> Credentials:
-    credentials = get_credentials_by_credentials_id(credentials_id)
-    db.delete(credentials)
-    db.commit()
-    db.close()
-    return credentials
+    return Credentials()
