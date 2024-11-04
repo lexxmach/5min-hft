c2765c1bcc9f9138b1fc35ebbe35c2c2b435a68e
Egor Molchan <egobars@yandex.ru>
2024-11-04 11:58:10 +0300

    front-fix

--- front/src/index.js
+++ front/src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <AnimatedBackground animationName="geometricShapes" />
     <BrowserRouter>
         <Routes>
@@ -19,7 +19,7 @@ root.render(
             <Route path='/question' element={<Question />} />
         </Routes>
     </BrowserRouter>
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
